902792a5c07d40de426b041bbb0a589ea9508a42
Aurelien Verla <aurelien.verla@seraphin.legal>
2023-06-20 22:14:37 +0200

    remove boilerplate comments

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,7 +9,3 @@ const config = {
     },
 };
 export default config;
-
-// To customize your Vite configuration you can use the viteFinal field.
-// Check https://storybook.js.org/docs/react/builders/vite#configuration
-// and https://nx.dev/packages/storybook/documents/custom-builder-configs
